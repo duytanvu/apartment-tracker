6352e6e86952933b24ea7ca8538c6fa6da9f949d
Tan Vu <duytanvu@icloud.com>
2021-01-12 23:34:52 -0500

    Send request to Zillow and get all links

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+chrome_driver_path = "C:\Development\ChromeDriver\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+response = requests.get(
+    "https://www.zillow.com/oshawa-on/rentals/1.5-_baths/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22Oshawa%2C%20ON%22%2C%22mapBounds%22%3A%7B%22west%22%3A-78.92746049260771%2C%22east%22%3A-78.8114174017874%2C%22south%22%3A43.857239699442744%2C%22north%22%3A43.95642829544949%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A792846%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22min%22%3A0%2C%22max%22%3A605683%7D%2C%22mp%22%3A%7B%22min%22%3A0%2C%22max%22%3A2000%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22baths%22%3A%7B%22min%22%3A1.5%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A13%7D",
+    headers=header
+)
 
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+all_link_elements = soup.select(".list-card-top a")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+all_links = []
+for link in all_link_elements:
+    href = link["href"]
+    print(href)
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
